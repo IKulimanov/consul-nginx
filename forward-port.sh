886638dbf09a653704d03f7b0d3b318a7e3a0eb7
IKulimanov <ivan.kulimanov@gmail.com>
2021-01-17 00:39:46 +0300

    shell enable

--- forward-port.sh
+++ forward-port.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 sudo iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
 sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
 sudo iptables -t nat -A OUTPUT -d localhost -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
 sudo iptables -t nat -A OUTPUT -d localhost -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
+
